76623c9d18b88ad874075e78b381b40b552bcbc3
Stoleee <sokolovstojan18@gmail.com>
2023-02-28 12:16:36 +0100

    krca

--- uniforme/index.php
+++ uniforme/index.php
@@ -16,16 +16,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo "$title";?></title>
-    <link rel="stylesheet" href="style.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
     <?php require_once "nav.php"; ?>
 
-    <div>
+    <div class="naslovnacnt">
       <?php
         if(isset($_GET['vrsta'])){
           $vrsta = $_GET['vrsta'];
@@ -48,10 +49,9 @@
                     <p>{$row['cena']}</p>
                   </div>";
             }
-
         } else {
-          echo  "<a href='index.php?vrsta=bluze'><img src='artikli/artikalM1' alt=''></a>";
-          echo  "<a href='index.php?vrsta=bluze'><img src='artikli/artikal1' alt=''></a>";
+          echo  "<a class='naslovna' href='index.php?vrsta=bluze'><img src='artikli/artikalM1' alt=''></a>";
+          echo  "<a class='naslovna' href='index.php?vrsta=bluze'><img src='artikli/artikal1' alt=''></a>";
         }
       ?>
     </div>
