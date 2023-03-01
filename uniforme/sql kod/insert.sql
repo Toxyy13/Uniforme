5d674d0442c3add7f9c39acd8d097ce358076d1d
Stoleee <sokolovstojan18@gmail.com>
2023-03-01 21:16:34 +0100

    Sredjene muske bluze

--- uniforme/sql kod/insert.sql
+++ uniforme/sql kod/insert.sql
@@ -28,24 +28,24 @@ INSERT INTO velicine(`size`) VALUES
                     ('3XL');
 
 INSERT INTO artikal(`sifra`,`naziv`,`vrsta_id`,`pol_id`,`cena`,`2XS`,`XS`,`S`,`M`,`L`,`XL`,`2XL`, `3XL`,`opis`) VALUES
-                   (NULL,'Muska bluza nitna1',8,1,5999,NULL,NULL,NULL,NULL,1,1,1,1,'Muska bluza na kopcanje sa dikerima i 3 dzepa. Sastav 35% pamuk, 65%poliester.'),
-                   (NULL,'Muska hirurska bluza01',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
-                   (NULL,'Muska hirurska bluza02',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
-                   (NULL,'Muska hirurska bluza03',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
-                   (NULL,'Muska hirurska bluza04',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
-                   (NULL,'Muska hirurska bluza05',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
-                   (NULL,'Muska bluza SUPERSTRETCH01',8,1,5999,1,1,1,1,1,1,NULL,1,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
-                   (NULL,'Muska bluza SUPERSTRETCH02',8,1,5999,1,1,NULL,NULL,1,NULL,1,1,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
-                   (NULL,'Muska bluza SUPERSTRETCH03',8,1,5999,1,1,1,1,1,1,NULL,NULL,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
-                   (NULL,'Muska bluza SUPERSTRETCH04',8,1,5999,NULL,1,NULL,NULL,1,1,1,1,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
-                   (NULL,'Muska bluza SUPERSTRETCH05',8,1,5999,1,1,1,1,1,1,NULL,NULL,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
-                   (NULL,'Muske pantalone01',9,1,5999,NULL,NULL,1,1,1,1,1,1,'Produzeni model pantalone sa lastizom, sa 2 dzepa. Sastav 35%pamuk i 65%poliester'),
-                   (NULL,'Muske pantalone02',9,1,5999,NULL,NULL,1,1,1,1,1,1,'Produzeni model pantalone sa lastizom, sa 2 dzepa. Sastav 35%pamuk i 65%poliester'),
-                   (NULL,'Muske pantalone03',9,1,5999,NULL,NULL,1,1,1,1,1,1,'Produzeni model pantalone sa lastizom, sa 2 dzepa. Sastav 35%pamuk i 65%poliester');
+                   (NULL,'Muska bluza nitna',8,1,5999,NULL,NULL,NULL,NULL,1,1,1,1,'Muska bluza na kopcanje sa dikerima i 3 dzepa. Sastav 35% pamuk, 65%poliester.'),
+                   (NULL,'Muska hirurska bluza',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
+                   (NULL,'Muska hirurska bluza',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
+                   (NULL,'Muska hirurska bluza',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
+                   (NULL,'Muska hirurska bluza',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
+                   (NULL,'Muska hirurska bluza',8,1,5999,NULL,NULL,1,1,1,1,1,1,'Muska hirurska bluza na V sa 3 dzepa. Sastav 35%pamuk, 65%poliester'),
+                   (NULL,'Muska bluza SUPERSTRETCH',8,1,5999,1,1,1,1,1,1,NULL,1,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
+                   (NULL,'Muska bluza SUPERSTRETCH',8,1,5999,1,1,NULL,NULL,1,NULL,1,1,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
+                   (NULL,'Muska bluza SUPERSTRETCH',8,1,5999,1,1,1,1,1,1,NULL,NULL,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
+                   (NULL,'Muska bluza SUPERSTRETCH',8,1,5999,NULL,1,NULL,NULL,1,1,1,1,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
+                   (NULL,'Muska bluza SUPERSTRETCH',8,1,5999,1,1,1,1,1,1,NULL,NULL,'Lagana bluza koja pruza neverovatan osecaj lakoce i udobnosti. Sastav bluze: 60%pamuk, 35%poliester i 5%elastin'),
+                   (NULL,'Muske pantalone',9,1,5999,NULL,NULL,1,1,1,1,1,1,'Produzeni model pantalone sa lastizom, sa 2 dzepa. Sastav 35%pamuk i 65%poliester'),
+                   (NULL,'Muske pantalone',9,1,5999,NULL,NULL,1,1,1,1,1,1,'Produzeni model pantalone sa lastizom, sa 2 dzepa. Sastav 35%pamuk i 65%poliester'),
+                   (NULL,'Muske pantalone',9,1,5999,NULL,NULL,1,1,1,1,1,1,'Produzeni model pantalone sa lastizom, sa 2 dzepa. Sastav 35%pamuk i 65%poliester');
 
 INSERT INTO slika(`put`,`artikal_id`) VALUES
                  ("artikli/Muska bluza nitna1.jpg", 1),
-                 ("artikli/Muska hirurska bluza01.jpg", 2),
+                 ("artikli/Muska hirurska bluza1.jpg", 2),
                  ("artikli/Muska hirurska bluza02.jpg", 3),
                  ("artikli/Muska hirurska bluza03.jpg", 4),
                  ("artikli/Muska hirurska bluza04.jpg", 5),
